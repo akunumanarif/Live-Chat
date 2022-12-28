e1a4f1c0ce5eb4cb2dcb9402312f883927837853
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-28 07:11:13 +0700

    Update Help Widg

--- src/components/HelpWidget.tsx
+++ src/components/HelpWidget.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { ChatPannel } from "./ChatPanel";
+
+export type TMessage = {
+    message: string;
+    id: string;
+    sender: string;
+  };
 
 export const HelpWidget = () => {
-    return (
-        <><div>Help</div></>
-    )
+    const [isChatPanelDisplayed, setIsChatPanelDisplayed] = useState(false);
+    return isChatPanelDisplayed ? (
+        <div
+          className="
+          fixed bottom-10 right-10
+          flex h-96 w-72 flex-col justify-between bg-white p-6"
+        >
+          {/* <ChatPannel
+            text={text}
+            setText={setText}
+            messages={messages}
+            onClose={handleCloseWidget}
+            handleSendMessage={handleSendMessage}
+          /> */}
+        </div>
+      ) : (
+        <button
+        //   onClick={handleOpenSupportWidget}
+          className="
+            fixed bottom-10 right-10 cursor-pointer bg-blue-400 p-2 px-4
+            text-white hover:bg-blue-500
+          "
+        >
+          Speak to us
+        </button>
+      );
 }
 
